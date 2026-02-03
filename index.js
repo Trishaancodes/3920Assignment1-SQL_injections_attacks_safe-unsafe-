d81a52f740d79e3c5a55962d74ae3e580c7f69f8
Trishaancodes <tshetty1@my.bcit.ca>
2026-02-02 19:17:18 -0800

    Handle SQL connection error gracefully
    Log an error message instead of throwing an error when SQL connection fails.

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ function logUsersTable(context) {
 }
 
 sqlConnection.query(sqlTable,(err, result) => {
-    if (err) throw err;
+    if (err) {
+      console.error('Unable to connect to sql');
+    };
     console.log("✅ SQL Table created or already exists.");
     logUsersTable("after table creation");
 
