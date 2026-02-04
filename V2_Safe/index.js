0008674e345d3af9c9f055bd59db1b6214a8782e
Trishaan <tshetty1@my.bcit.ca>
2026-02-03 18:27:02 -0800

    made port flexible

--- V2_Safe/index.js
+++ V2_Safe/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const session = require('express-session');
 const bcrypt = require('bcrypt');
 const MongoStore = require('connect-mongo');
